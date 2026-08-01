00c75dedd2d58255b4efec7db00bcc2d014e6574
Aboli Sadavarte <abolisadavarte07@gmail.com>
2026-08-01 14:51:39 +0530

    Handle duplicate filenames during organization

--- organizer/utils.go
+++ organizer/utils.go
@@ -1,11 +1,15 @@
 package organizer
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-// MoveFile moves a file into Organized/<Category>
+// MoveFile moves a file into Organized/<Category>.
+// If a file with the same name already exists,
+// it creates "filename - Copy.ext", "filename - Copy (2).ext", etc.
 func MoveFile(basePath string, file FileInfo, category string) error {
 
 	// Destination folder
@@ -21,6 +25,33 @@ func MoveFile(basePath string, file FileInfo, category string) error {
 
 	// Destination path
 	destination := filepath.Join(destFolder, file.Name)
+
+	// If destination already exists, create a new filename
+	if _, err := os.Stat(destination); err == nil {
+
+		ext := filepath.Ext(file.Name)
+		name := strings.TrimSuffix(file.Name, ext)
+
+		i := 1
+
+		for {
+			var newName string
+
+			if i == 1 {
+				newName = fmt.Sprintf("%s - Copy%s", name, ext)
+			} else {
+				newName = fmt.Sprintf("%s - Copy (%d)%s", name, i, ext)
+			}
+
+			destination = filepath.Join(destFolder, newName)
+
+			if _, err := os.Stat(destination); os.IsNotExist(err) {
+				break
+			}
+
+			i++
+		}
+	}
 
 	// Move file
 	return os.Rename(source, destination)
